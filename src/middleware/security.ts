b656e3a9b74cbc0080b92b294089d39f9f711344
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2026-01-29 11:54:07 +0300

    implement security middleware

---
+++ src/middleware/security.ts
@@ -0,0 +1,73 @@
+import type { Request, Response, NextFunction } from "express";
+import type { RateLimitRole } from "../type.js";
+import  aj  from "../config/arcjet.js";
+import { slidingWindow, ArcjetNodeRequest} from "@arcjet/node";
+
+const securityMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if(process.env.NODE_ENV === "test") return next();
+
+    try {
+        const role: RateLimitRole = req.user?.role || "guest";
+
+        let limit: number;
+        let message: string;
+
+        switch (role) {
+            case "admin":
+                limit=20;
+                message="Admin request limit exceeded (20 per minute). Please try again later.";
+                break;
+            case "teacher":
+                limit=10;
+                message="Teacher request limit exceeded (10 per minute). Please try again later.";
+                break;
+            case "student":
+                limit=10;
+                message="Student request limit exceeded (10 per minute). Please try again later.";
+                break;
+            case "guest":
+                limit=2;
+                message="Guest request limit exceeded (2 per minute). Sign in to increase your limit.";
+                break;
+        }
+
+        const client = aj.withRule(
+            slidingWindow({
+                mode: 'LIVE',
+                interval: '1m',
+                max: limit,
+            })
+        )
+
+        const arcjetRequest: ArcjetNodeRequest = {
+            headers: req.headers,
+            method: req.method,
+            url: req.originalUrl ?? req.url,
+            socket: {
+                remoteAddress: req.socket.remoteAddress ?? req.ip ?? '0.0.0.0'
+            },
+        }
+
+        const decision = await client.protect(arcjetRequest);
+
+        if (decision.isDenied() && decision.reason.isBot()) {
+            return res.status(403).json({ error: 'Forbidden' ,message: "Automated requests are denied." });
+        }
+        if (decision.isDenied() && decision.reason.isShield()) {
+            return res.status(403).json({ error: 'Forbidden' ,message: "Request blocked by security rules." });
+        }
+        if (decision.isDenied() && decision.reason.isRateLimit()) {
+            return res.status(403).json({ error: 'Forbidden' ,message: "Rate limit exceeded." });
+        }
+
+        next();
+
+
+    }catch (error) {
+        console.error("Security middleware error:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+
+}
+
+export default securityMiddleware;
